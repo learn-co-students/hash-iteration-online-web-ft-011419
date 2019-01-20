3efa08db5ef4c586132a97b6a192f7874bf96eb4
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-20 02:54:07 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,9 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27
+"Timmy" => 9,
+"Sarah" => 6,
+"Amanda" => 27
+}
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+happy_birthday(birthday_kids)
+  puts "Happy Birthday #{"Sarah"}! You are now #{6} years old!"
 end
-
-
